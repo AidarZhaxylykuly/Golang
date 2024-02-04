1ff38fe8298dd32f1c72cd29fcf56d7a9f34f34a
Aidar <a_zhaxylykuly@kbtu.kz>
2024-02-04 23:43:30 +0600

    Last Commit

--- TSIS1/pkg/handlers/dailyex.go
+++ TSIS1/pkg/handlers/dailyex.go
@@ -1,24 +1,22 @@
 package handlers
 
-import (
-	"strings"
-)
-type Day struct{
-	Number int 'json:"number"'
-	NameoftheDay string 'json:"nameoftheday"'
-	Lessons string 'json:"lessons"'
-	HomeWork string 'json:"homework"'
-	Leisure string 'json:leisure'
+
+type Day struct {
+    Number       int    `json:"number"`
+    NameoftheDay string `json:"nameoftheday"`
+    Lessons      string `json:"lessons"`
+    HomeWork     string `json:"homework"`
+    Leisure      string `json:"leisure"`
 }
 
-var Days []Day{
-	{NameoftheDay: "Monday", Lessons: "Golang\nElectronics&DD\nKazakh", HomeWork:"Optional(Which lesson has to be done immediately)", leisure:"None"},
-	{NameoftheDay: "Tuesday", Lessons: "OOP", HomeWork:"Web Development", leisure:"Drawing Hobby"},
-	{NameoftheDay: "Wednesday", Lessons: "WebDev\nOOP\nKazakh", HomeWork:"Optional(Which lesson has to be done immediately)", leisure:"None"},
-	{NameoftheDay: "Thursday", Lessons: "WebDev\nElectronics&DD", HomeWork:"ICT", leisure:"None"},
-	{NameoftheDay: "Friday", Lessons: "Kazakh", HomeWork:"Electronics&DD", leisure:"Drawing Hobby"},
-	{NameoftheDay: "Saturday", Lessons: "ICT\nGolang", HomeWork:"OOP", leisure:"None"},
-	{NameoftheDay: "Sunday", Lessons: "ICT", HomeWork:"Golang", leisure:"Football: Liverpool v Arsenal"}
+var Days = []Day{
+	{NameoftheDay: "Monday", Lessons: "Golang\nElectronics&DD\nKazakh", HomeWork: "Optional(Which lesson has to be done immediately)", Leisure: "None"},
+	{NameoftheDay: "Tuesday", Lessons: "OOP", HomeWork: "Web Development", Leisure: "Drawing Hobby"},
+	{NameoftheDay: "Wednesday", Lessons: "WebDev\nOOP\nKazakh", HomeWork: "Optional(Which lesson has to be done immediately)", Leisure: "None"},
+	{NameoftheDay: "Thursday", Lessons: "WebDev\nElectronics&DD", HomeWork: "ICT", Leisure: "None"},
+	{NameoftheDay: "Friday", Lessons: "Kazakh", HomeWork: "Electronics&DD", Leisure: "Drawing Hobby"},
+	{NameoftheDay: "Saturday", Lessons: "ICT\nGolang", HomeWork: "OOP", Leisure: "None"},
+	{NameoftheDay: "Sunday", Lessons: "ICT", HomeWork: "Golang", Leisure: "Football: Liverpool v Arsenal"},
 }
 
 func AllWeeklyTasks() []Day{
